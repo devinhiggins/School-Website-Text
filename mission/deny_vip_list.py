1a1ebc9f5c162518478cdd40888827144aa1c49f
jhp <jhp@msu.edu>
2021-07-02 06:13:10 -0700

    list of keywords, terms, and urls used in processing

---
+++ mission/deny_vip_list.py
@@ -0,0 +1,52 @@
+# list of keywords, terms, and urls used in the process
+
+priority_keys = ['About', 'About us', 'Mission', 'Vision', 'Our school', 'Our mission',
+                 'Principal', 'message', 'School info']
+
+traverse_triggers = ['About', 'About Us', 'Our School']
+
+filtered_keys = ['Contact', 'Calendar', 'Site', 'Map', 'Facebook', 'Login', 'Privacy', 'Policy', 'Enroll',
+                 'Transportation', 'Student', 'Language', 'Select', 'Governing', 'Meeting', 'Sign', 'Athletic',
+                 'Staff', 'Directory', 'More', 'News', 'Direction', 'Health', 'Virtual', 'Showcase', 'Art',
+                 'Department', 'Community', 'Parent', 'Enrollment', 'Accessibility', 'Academic', 'Program', 'Twitter',
+                 'Skip', 'Content', 'Employ', 'Activities', 'Webmaster', 'View', 'Enroll', 'Tax', 'Activity',
+                 'Resource', 'Last', 'Nondiscrimination', 'Register', 'Event', 'PTO', 'Menu', 'Donation', 'Donate',
+                 'Career', 'Translate', 'Schedule', 'Bus', 'Back', 'Power', 'Admin', 'Apply', 'Architeck', 'Child',
+                 'Helpful', 'Instagram', 'Read', 'Cafe', 'Portal', 'Share', 'Library', 'Term', 'Salary', 'Password',
+                 'Tip', 'Safe', 'Tweet', 'Photo', 'Registration', 'Faculty', 'Click', 'Reply']
+
+deny_list = ['zoom.us', 'youtu.be', 'mailto', 'maps.google.com', '.pdf', 'schoolwires.com', 'blackboard.com', 'tel:',
+             'javascript:', '.jpg', "jostens.com", "ziprealty.com", "postmates.com", "doordash.com", "seamless.com",
+             "ubereats.com", "grubhub.com", "defense.gov", "reaw.us", "primeclerk.com", "myallsavers.com", '.docx',
+             "mdreducation.com", "schools-info.com", "nuwber.com", "800zipcodes.com", "https://www.schoolsnearme.net",
+             "manta.com", "trueschools.com", "citydirectory.us", "joinmosaic.com", "coldwellbanker.com", "noblenet.org",
+             "allpublicschools.org", "healthzaa.com", "books.google.com", "ellsworthamerican.com", "xome.com",
+             "tidesandcurrents.noaa.gov", "illinoisreportcard.com", "redfin.com", "nationalblueribbonschools.ed.gov",
+             "huggingface.co", "issuu.com", "zipcoder.com", "neighborhoodscout.com", "educationbug.org", ".har.com",
+             "apartmenthomeliving.com", "edunify.org", "tripadvisor.com", "alumnius.net", "towncharts.com", '.xlsx',
+             "amp.blog.shops-net.com", "educationcenters.com", "mymapplan.com", "prepsportswear.com", "yelp.com",
+             "collegehelper411.com", "schoolsineachstate.com", "hotels4teams.com", "edussori.com", "twinkl", '.xls',
+             "50states.com", "lensa.com", "youtube.com", "instagram.com", "publicschoolsk12.com", "practutor.com",
+             "accounts.google.com", "support.google.com", "policies.google.com", "myactivity.google.com", '.pptx',
+             "business.google.com", "www.google.com", "cde.ca.gov", "twitter.com", "wikipedia.org", "bestplaces.net",
+             "city-data.com", "facebook.com", "usnews.com", "greatschools.org", "niche.com", "publicschoolreview.com",
+             "schooldigger.com", "texastribune.org", "nces.ed.gov", "k12guides.com", "high-schools.com", '.doc', '.ppt',
+             "hometownlocator.com", "mapquest.com", "newleafpropertymanagement.com", "psychologytoday.com", '.ics',
+             "realtor.com", "zillow.com", "neighborhoods.com", "apartments.com", "forrent.com", "apartmentfinder.com",
+             "insideschools.org", "newyorkschools.com", "areavibes.com", "collegesimply.com", "maxpreps.com", 'rss?',
+             "211cny.com", "propublica.org", "movoto.com", "homesnap.com", "newsday.com", "teacherlists.com",
+             "elementaryschools.org", "yellowpages.com", "trulia.com", "schooltimeline.com", "hisawyer.com",
+             "spellingcity.com", "agile-ed.com", "century21.com", "k12academics.com", "charitynavigator.org",
+             "alignable.com", "linkedin.com", "guidestar.org", 'drive.google.com', '/maps', 'Download', 'getfile']
+
+mission_terms = ['academic success', 'academic excellence', 'educational excellence', 'pursuit of high standards',
+                 'student achievement', 'academic', 'maximum potential', 'future success', 'caring environment',
+                 'nurture', 'challenge', 'challenging', 'high-quality education', 'rigorous educational experiences',
+                 'citizenship', 'citizen', 'leaders', 'collaborative', 'collaborating', 'commitment', 'empower',
+                 'empowerment', 'life-long learning', 'life-long learners', 'opportunity', 'partnership',
+                 'combined effort', 'physical', 'physically', 'productive', 'productivity', 'responsible',
+                 'responsibility', 'safe environment', 'socially', 'social development', 'social', 'community',
+                 'cooperation with the community', 'community centered', 'global society', 'changing world',
+                 'value of diversity', 'diversity', 'ever changing', 'soft skill', 'manner', 'perseverance',
+                 'emotional skill', 'self-discipline', 'self-esteem', 'values', 'self-worth', 'confidence',
+                 'cultivating individual strength', 'respect for individuality', 'accommodating individual differences']
